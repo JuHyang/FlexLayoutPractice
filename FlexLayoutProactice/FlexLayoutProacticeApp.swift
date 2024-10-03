ee1a966dfd2b00f98eb469878784e8235df1e306
JuHyang <ljh28891004@gmail.com>
2024-10-03 22:53:10 +0900

    Initial Commit

---
+++ FlexLayoutProactice/FlexLayoutProacticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexLayoutProacticeApp.swift
+//  FlexLayoutProactice
+//
+//  Created by juhyang on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FlexLayoutProacticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
